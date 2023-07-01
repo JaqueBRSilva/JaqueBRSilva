b71712af2aecf665a39f2179844b79450b9feb07
JaqueBRSilva <j.silvabroccolo@gmail.com>
2023-07-01 14:59:00 -0300

    feat: add imports no `app-module`, e seções em `home`

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopMenuComponent } from './components/top-menu/top-menu.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
+import { GithubStatusComponent } from './pages/github-status/github-status.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PresentationComponent } from './pages/presentation/presentation.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopMenuComponent,
+    HomeComponent,
+    AboutComponent,
+    PresentationComponent,
+    GithubStatusComponent,
+    ExperienceComponent,
+    ProjectsComponent,
   ],
   imports: [
     BrowserModule,
