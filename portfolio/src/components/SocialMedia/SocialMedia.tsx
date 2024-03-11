b9cb13af050d354f26351735b35be79467fd2342
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2024-03-10 23:14:26 -0300

    refactor: rem. links das redes sociais p/ arq. dedicado

--- portfolio/src/components/SocialMedia/SocialMedia.tsx
+++ portfolio/src/components/SocialMedia/SocialMedia.tsx
@@ -1,38 +1,19 @@
 import React from 'react'
+import { SOCIAL_MEDIA_LINKS } from '../../data/SocialMedia'
 import './SocialMedia.sass'
 
 const SocialMedia: React.FC = () => {
 
-    const CURRICULUM_URL = 'https://drive.google.com/file/d/1f-M1jWZnygiDvMhnElIhPGT2Uhf8Oi7a/view?usp=sharing'
-    const GITHUB_URL = "https://github.com/JaqueBRSilva"
-    const LINKEDIN_URL = "https://www.linkedin.com/in/jaqueline-silva-broccolo/"
-    const EMAIL_URL = "mailto:j.silvabroccolo@gmail.com"
-    const BIOLINK_URL = "https://bio.link/jaquebrsilva"
-
     return (
         <div className='social_media_icons'>
 
-            <a href={CURRICULUM_URL} target="_blank" rel='noopener noreferrer'>
-                <img className="socials" src="https://i.ibb.co/W5mqKz9/download-resume-96.png" alt="visualizar currículo" />
-            </a>
+            {SOCIAL_MEDIA_LINKS.map((item) => (
+                <a href={item.url} target="_blank" rel='noopener noreferrer'>
+                    <img className="socials" src={item.icon_source} alt={item.link_name} />
+                </a>
+            ))}
 
-            <a href={GITHUB_URL} target="_blank" rel="noopener noreferrer">
-                <img className="socials" src="https://i.ibb.co/t3x3BFY/github-96.png" alt="github" />
-            </a>
-
-            <a href={LINKEDIN_URL} target="_blank" rel="noopener noreferrer">
-                <img className="socials" src="https://i.ibb.co/JmkWkwK/linkedin-96.png" alt="linkedin" />
-            </a>
-
-            <a href={EMAIL_URL} target="_blank" rel='noopener noreferrer'>
-                <img className="socials" src="https://i.ibb.co/DWpTftZ/email-96.png" alt="e-mail" />
-            </a>
-
-            <a href={BIOLINK_URL} target="_blank" rel='noopener noreferrer'>
-                <img className="socials" src="https://i.ibb.co/QP2VGxx/link-96.png" alt="bio.link" />
-            </a>
-
-        </div >
+        </div>
     )
 }
 
---
+++ portfolio/src/data/SocialMedia.ts
@@ -0,0 +1,31 @@
+export const SOCIAL_MEDIA_LINKS = [
+    {
+        "link_name": "Currículo",
+        "url": "https://drive.google.com/file/d/1f-M1jWZnygiDvMhnElIhPGT2Uhf8Oi7a/view?usp=sharing",
+        "icon_source": "https://i.ibb.co/W5mqKz9/download-resume-96.png"
+    },
+
+    {
+        "link_name": "GitHub",
+        "url": "https://github.com/JaqueBRSilva",
+        "icon_source": "https://i.ibb.co/t3x3BFY/github-96.png"
+    },
+
+    {
+        "link_name": "LinkedIn",
+        "url": "https://www.linkedin.com/in/jaqueline-silva-broccolo/",
+        "icon_source": "https://i.ibb.co/JmkWkwK/linkedin-96.png"
+    },
+
+    {
+        "link_name": "Email",
+        "url": "mailto:j.silvabroccolo@gmail.com",
+        "icon_source": "https://i.ibb.co/DWpTftZ/email-96.png"
+    },
+
+    {
+        "link_name": "bio.link",
+        "url": "https://bio.link/jaquebrsilva",
+        "icon_source": "https://i.ibb.co/QP2VGxx/link-96.png"
+    },
+]
