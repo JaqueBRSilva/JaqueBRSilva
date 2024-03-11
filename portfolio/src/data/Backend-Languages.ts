1317ccfc3847f95126ddc8c5bccb22d373f93eac
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2024-03-10 23:27:35 -0300

    feat: add listas de linguagens

---
+++ portfolio/src/data/Backend-Languages.ts
@@ -0,0 +1,27 @@
+export const BACKEND_LANGUAGES = [
+    {
+        "lang_name": "Node.js",
+        "url_icon": "https://i.ibb.co/GnY7szq/node-js-logo.png"
+    },
+
+    {
+        "lang_name": "Express.js",
+        "url_icon": ""
+    },
+
+    {
+        "lang_name": "MongoDB",
+        "url_icon": ""
+    },
+
+    // {
+    //     "lang_name": "NestJS",
+    //     "url_icon": ""
+    // },
+
+    // {
+    //     "lang_name": "Swagger",
+    //     "url_icon": ""
+    // },
+
+]
