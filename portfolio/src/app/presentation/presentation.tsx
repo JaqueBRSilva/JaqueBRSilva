1aa2f74060a289859330b97e7dc630b184500652
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2024-03-10 22:12:53 -0300

    refactor: troca de link por variável, componente movido p/ bloco

--- portfolio/src/app/presentation/presentation.tsx
+++ portfolio/src/app/presentation/presentation.tsx
@@ -3,20 +3,22 @@ import './presentation.sass'
 
 export default function Presentation() {
 
+    const PHOTO_URL = "https://avatars.githubusercontent.com/u/23319453?v=4"
+
     return (
         <div className="background_layer">
             <div className="container_profile">
 
                 <div className="photo_area">
-                    <img className="github_photo" src="https://avatars.githubusercontent.com/u/23319453?v=4" alt="Foto do GitHub" />
+                    <img className="github_photo" src={PHOTO_URL} alt="Foto do GitHub" />
                 </div>
 
                 <div className="profile_area">
                     <h1>JAQUELINE SILVA BROCCOLO</h1>
                     <h2>Desenvolvedora Front-end</h2>
-                </div>
 
-                <SocialMedia />
+                    <SocialMedia />
+                </div>
 
             </div>
         </div>
