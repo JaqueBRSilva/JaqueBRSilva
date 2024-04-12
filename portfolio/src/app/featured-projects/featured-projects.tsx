e4f093e40b5cc8633b2e4e113941a7fe3086abc5
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2024-04-12 00:34:12 -0300

    refactor: trecho de 'featured-projects' transf. em componente

--- portfolio/src/app/featured-projects/featured-projects.tsx
+++ portfolio/src/app/featured-projects/featured-projects.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import ProjectsList from '../../components/ProjectsList/ProjectsList'
 import { MY_FEATURED_PROJECTS } from '../../data/MyFeaturedProjects'
 import { IFeaturedProjectsData } from '../../types/Featured-Projects'
 import './featured-projects.sass'
@@ -17,48 +18,10 @@ export default function FeaturedProjects() {
             <div className="featured-main-container">
                 <div className="featured-cards-list">
 
-                    {projectsList.map((proj: IFeaturedProjectsData) => (
+                    <ProjectsList
+                        list={projectsList}
+                    />
 
-                        <div id={proj.id} className="featured-repository-cards">
-                            <img className="featured-preview-image"
-                                src={proj.url_preview_img}
-                                alt={proj.project_name}
-                            />
-
-                            <div className="featured-project-name-container">
-                                <a href={proj.url_demo_link} target="blank" rel="noopener noreferrer">
-                                    <div className="featured-new-window-icon">
-                                        <p className="featured-project-name">
-                                            {proj.project_name}
-                                        </p>
-
-                                        <img
-                                            className="open-in-new-window-icon"
-                                            src="https://img.icons8.com/material-outlined/48/16b0e4/open-in-window.png"
-                                            alt="open-in-window"
-                                        />
-                                    </div>
-                                </a>
-                                <p className='featured-project-type'>
-                                    {proj.project_type}
-                                </p>
-                            </div>
-
-                            <p className="featured-repo-description">
-                                {proj.description}
-                            </p>
-
-                            <div className='featured-repo-main-languages-container'>
-                                {proj.used_languages.map(lang => (
-                                    <p className="featured-repo-main-languages">
-                                        {lang}
-                                    </p>
-                                ))}
-                            </div>
-
-                        </div>
-                    ))
-                    }
                 </div>
             </div>
         </>
---
+++ portfolio/src/components/ProjectsList/ProjectsList.tsx
@@ -0,0 +1,75 @@
+import { IFeaturedProjectsData } from "../../types/Featured-Projects"
+import './ProjectsList.sass'
+
+interface SelectedProjectsProps {
+    list: []
+}
+
+const ProjectsList = ({ ...props }: SelectedProjectsProps) => {
+
+    const OPEN_IN_WINDOW_ICON = 'https://img.icons8.com/material-outlined/48/16B0E4/open-in-window.png'
+    const GITHUB_ICON = "https://img.icons8.com/material-outlined/48/16B0E4/github.png"
+
+    return (
+        <>
+            {
+                props.list.map((proj: IFeaturedProjectsData) => (
+
+                    <div id={proj.id} className="featured-repository-cards">
+                        <img className="featured-preview-image"
+                            src={proj.url_preview_img}
+                            alt={proj.project_name}
+                        />
+
+                        <div className="featured-project-name-container">
+                            <p className="featured-project-name">
+                                {proj.project_name}
+                            </p>
+
+                            <div className="featured-icons-container">
+                                <a target="_blank" rel="noopener noreferrer"
+                                    href={proj.url_demo_link}
+                                >
+                                    <img className="open-in-new-window-icon"
+                                        src={OPEN_IN_WINDOW_ICON}
+                                        alt="open-in-window"
+                                    />
+                                </a>
+
+                                <a target="_blank" rel="noopener noreferrer"
+                                    href={proj.url_github_link}
+                                >
+                                    <img className="featured-github-logo"
+                                        src={GITHUB_ICON}
+                                        alt="github logo"
+                                    />
+                                </a>
+                            </div>
+                        </div>
+
+                        <div>
+                            <p className='featured-project-type'>
+                                {proj.project_type}
+                            </p>
+                        </div>
+
+                        <p className="featured-repo-description">
+                            {proj.description}
+                        </p>
+
+                        <div className='featured-repo-main-languages-container'>
+                            {proj.used_languages.map(lang => (
+                                <p className="featured-repo-main-languages">
+                                    {lang}
+                                </p>
+                            ))}
+                        </div>
+
+                    </div>
+                ))
+            }
+        </>
+    )
+}
+
+export default ProjectsList
